4aa2a573cd191f3891655d8f4ac410ba29e50e1f
MHWatchy <mwatchy@gmail.com>
2023-12-11 14:58:54 +0000

    Single article pages created, very basic styling

--- src/utils/fetches.js
+++ src/utils/fetches.js
@@ -3,3 +3,9 @@ import axios from "axios"
 const newsApi = axios.create({
   baseURL: "https://news-server-k1ka.onrender.com/api",
 })
+
+export const getSingleArticle = (id) => {
+  return newsApi.get(`/articles/${id}`).then(({data}) => {
+    return data
+  })
+}
